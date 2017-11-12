e2cb7b76e448a227fb76dd8510db633be2514910
Bryan Kho <bryank1991@hotmail.com>
2017-11-12 21:51:50 +0800

    Added more changes
    Loads of changes. Documentation inside file

--- scandownloadv2_windows.py
+++ scandownloadv2_windows.py
@@ -1,6 +1,19 @@
 #!python3.6
 
-import requests, sys, webbrowser, bs4, re
+import requests, sys, webbrowser, bs4, re, os
+# from balloontip.py import WindowsBalloonTip
+
+from win10toast import ToastNotifier
+
+'''
+toaster = ToastNotifier()
+toaster.show_toast("Hello World!!!",
+              "Python is 10 seconds awsm!",
+              duration=1)
+toaster.show_toast("Hello World!!!",
+             "Python is awsm by default!")
+'''
+
 
 #print 'Number of arguments:', len(sys.argv), 'arguments.'
 #print 'Argument List:', str(sys.argv)
@@ -11,34 +24,62 @@ if (len(sys.argv) < 2):
 
 argv = sys.argv
 
-# grab link
-res = requests.get('https://eztv.unblocked.bid/search/' + argv[1])
-# res.raise_for_status()
+# we need to remove the first instance of an array as the array currently is grabbing nothing for some reason
+argv.pop(0)
 
-# Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text,'html.parser')
+# before proceeding with the meat of the code, we need to grab the file
+logFile = open('downloaded.txt')
+logFileText = logFile.read()
 
 
-# lets try find all links
-if 1: 
-	for link in soup.find_all('a', {'class':'download_1'}):
-		thisLink = link.get('href')
-		# print(thisLink)		
+for torrentLink in argv:
+    # we need to convert all arguments to use full stops instead to be used later
+    torrentLinkStrip = torrentLink.replace('-','.')
 
-		# need to find this link whether it contains magnet and matches the right string from the latest episode
-		if 1:
-		# if re.search('magnet',thisLink):
-			print("Found torrent link: \n" + thisLink + "\n")
-				
-			#searchArray = re.search('magnet',link.get('href'))
-		        #print(searchArray)
-		
-			r = requests.get(thisLink)
-			with open("filename", "wb") as code:
-			    code.write(r.content)
-			print("Download Complete!")
-	
+    res = requests.get('https://eztv.unblocked.bid/search/' + torrentLink)
+    # res.raise_for_status()
 
-			# lets exit on first one only
-			sys.exit()
+    # Retrieve top search result links.
+    soup = bs4.BeautifulSoup(res.text,'html.parser')
+
+    # lets try find all links
+    for link in soup.find_all('a', {'class':'download_1'}):
+        thisLink = link.get('href')
+
+        # lets split the word. once we've done that, we get the first 6 characters only
+        episodeText = thisLink.split(torrentLinkStrip)[1]
+        print('This is episode:' + episodeText[1:7])
+        episodeTextNumber = episodeText[1:7]
+
+        # combine both titles and episode to be used in the text file
+        torrentCompare = torrentLinkStrip + '.' + episodeTextNumber
+
+        # check if it exist in our download logger
+        if torrentCompare in open('downloaded.txt').read():
+
+            print('This torrent has been downloaded already:' + torrentCompare)
+            continue # go to next link
+
+        else:
+
+            if thisLink:
+
+                # we've found a link. time to download it by trying to open it
+                # if re.search('magnet',thisLink):
+                print("Found torrent link: \n" + thisLink + "\n")
+
+
+                # once we downloaded those files, we would need to record what was downloaded onto a file
+
+
+                '''
+                if 0:
+                    r = requests.get(thisLink)
+                    with open("filename", "wb") as code:
+                        code.write(r.content)
+                    print("Download Complete!")
+                '''
+
+            # lets exit on first one only
+            # sys.exit()
 			
