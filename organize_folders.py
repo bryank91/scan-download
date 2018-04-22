17ece8430edcfd11b5bb68e15c91d9d8cffda333
bryank91 <bryank1991@hotmail.com>
2018-04-22 21:34:50 +0800

    v1 release

--- organize_folders.py
+++ organize_folders.py
@@ -4,6 +4,7 @@ import shutil, os, subprocess, sys
 argv = sys.argv
 
 TVSHOWDIR = '/disks/hdd2/TV Shows'
+TVSHOWDIRESCAPE = '/disks/hdd2/TV\ Shows/'
 DOWNLOADDIR = '/var/lib/transmission-daemon/downloads'
 
 #os.chdir('/home/bryan/Documents/Python/scan-download')
@@ -14,38 +15,52 @@ DOWNLOADDIR = '/var/lib/transmission-daemon/downloads'
 os.chdir(DOWNLOADDIR)
 for fromfolder in os.listdir():
 
-	# grab the title and separating the periods to spaces
+        # grab the title and separating the periods to spaces
         foundseasontext = 0
         showtitle = ""
         namearray = fromfolder.split('.')[:-1]
 
-	# once we have those strings split into arrays, we now need to determine whether it's an episode and season
+        # once we have those strings split into arrays, we now need to determine whether it's an episode and season
         for word in namearray:
                 wordarray = list(word)	
+                print(wordarray)
 		
-		# check if it's a season/episode word
-                if wordarray[0] == 'S' and wordarray[1].isdigit == True:
-			# PASS but might need to more checks as we do not check for eps now. might fail if a title starts with S followed by a numeric value
+                # check if it's a season/episode word
+                if wordarray[0] == 'S' and wordarray[1].isdigit() == True:
+                        # PASS but might need to more checks as we do not check for eps now. might fail if a title starts with S followed by a numeric value
+                        print("Found Season")
                         foundseasontext = 1 
 	 
                 if foundseasontext:
-			# lets store all the words we had
-                        break
+                        # lets store all the words we had
+                        next
                 else:
-			# construct the title
+                        # construct the title
                         if showtitle: 
                                 showtitle = showtitle + " " + word
                         else:
                                 showtitle = word
-				
+
+        # we got the title time to use it to serach
+        
         print(showtitle)
 
-	
-os.chdir(TVSHOWDIR)
-for tofolder in os.listdir():
-	if "Young Sheldon" in tofolder:
-		print("success")
-		# We found the folder, now to get the directory
 
+if (1):	
+    os.chdir(TVSHOWDIR)
+    for tofolder in os.listdir():
+        if showtitle in tofolder:
+            print("Success: Found in" + TVSHOWDIR + "/" + showtitle)
 
-# proc = subprocess.Popen('touch /home/bryan/Documents/Python/scan-download', shell=True, ...)
+            fromdir = DOWNLOADDIR + "/" + fromfolder;
+
+            # we need to escape the folder
+            showtitleescape = showtitle.replace(" ","\ ")
+            todir = TVSHOWDIRESCAPE + "/" + showtitleescape + "/" + fromfolder
+
+            # os.rename(DOWNLOADDIR + "/" + fromfolder, TVSHOWDIR + "/" + fromfolder)
+            print("Trying to move" + fromdir + " TO " + todir )
+            # shutil.copyfile(fromdir,todir)
+            print('cp ' + fromdir + ' ' + todir)
+            os.popen('cp ' + fromdir + ' ' + todir).read()
+                
