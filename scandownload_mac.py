5c7efa828d699d7c090c99aa1c0c43fd95b20ca6
bryank91 <bryank1991@hotmail.com>
2018-04-26 21:33:38 +0800

    updated stuff

--- scandownload_mac.py
+++ scandownload_mac.py
@@ -1,10 +1,5 @@
 #! /usr/bin/python
 
-# TODO : Add another argument to accept season and episode (eg. S0301)
-# TODO : Only accept the lowest size file. SD probably
-# TODO
-
-# all the dependencies
 import requests, sys, webbrowser, bs4, urllib2, re
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
@@ -15,25 +10,16 @@ if (len(sys.argv) < 2) :
 	sys.exit()
 
 argv = sys.argv
-
 arglen = len(sys.argv) 
 
-
-# grab url of the webpage
 res = requests.get('https://eztv.unblocked.bid/search/' + argv[1])
 # res.raise_for_status()
 
-# Retrieve top search result links.
 soup = bs4.BeautifulSoup(res.text,'html.parser')
 
-
-# lets try find all links
 if 1: 
 	for link in soup.find_all('a', {'class':'download_1'}):
 		thisLink = link.get('href')
-
-		# need to find this link whether it contains magnet and matches the right string from the latest episode
-		# if re.search('magnet',thisLink):
 
 		if 1:
 			if arglen == 4: # only filetype is defined argv is defined
@@ -50,20 +36,10 @@ if 1:
 			# once found, further refine by displaying size and season	
 			
 			if 0:	
-				# only if you checked magnets 
-				#searchArray = re.search('magnet',link.get('href'))
-				#print(searchArray)
-		
 				r = requests.get(thisLink)
 				with open("/tmp/filename.torrent", "wb") as code:
 				    code.write(r.content)
 				print("Download Complete!");
 	
-
-				# lets exit on first one only
-				# sys.exit()
-		
-
-
 # back tick these functions next time
 # transmission-remote -n 'bryan:bryan' -a /var/lib/transmission-daemon/downloads/files.torrent	
