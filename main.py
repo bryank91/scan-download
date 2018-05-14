d11f5a86d708a80aa64e6b3332c8fe453ed5e051
bryank91 <bryank1991@hotmail.com>
2018-05-14 20:10:36 +0800

    updated main

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #! /usr/bin/env python3.5
 import shutil, os, subprocess, sys
+from check_ep import * 
 
-argv = sys.argv
+
+series = ["modern-family","young-sheldon","big-bang-theory","fresh-off-the-boat","westworld","silicon-valley"]
+serieswithdot = ["modern.family","young.sheldon","the.big.bang.theory","fresh.off.the.boat","westworld","silicon.valley"]
+
+for title in serieswithdot:
+	answer = ""
+	answer = getLatestEpisode('output.csv', title)
+	print("Title: " + title + " : " + str(answer))
+ 
 
